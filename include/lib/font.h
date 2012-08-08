05faa01eaeb4bfe2c2d7e0801d6924345a0dba64
geileszeuch <bestaetigungsaccount@gmx.de>
2012-08-08 15:15:41 +0300

    Update include/lib/font.h
    Pre3 Support

--- include/lib/font.h
+++ include/lib/font.h
@@ -3,9 +3,9 @@
 
 #include <lib/gfx.h>
 
-#define FONT_X	16
-#define FONT_Y	32
-#define FONT_COLS 2
+#define FONT_X	12
+#define FONT_Y	24
+#define FONT_COLS 1
 
 void font_draw_char(gfx_surface *surface, unsigned char c, int x, int y, uint32_t fg_color, uint32_t bg_color);
 
